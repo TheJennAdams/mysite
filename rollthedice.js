238aeb3a086a135878792c45ff89143db00e2704
TheJennAdams <110792460+TheJennAdams@users.noreply.github.com>
2022-08-20 22:17:40 -0600

    Update rollthedice.js

--- rollthedice.js
+++ rollthedice.js
@@ -14,10 +14,29 @@ var Player2 = prompt("Enter the name of Player Two");
 
 // Dice Roll
 function clickDiceRoll() {
-  document.querySelector(".img1").setAttribute("src", `/docs/assets/images/diceimages/dice${rollDice()}.png`)
-  document.querySelector(".img2").setAttribute("src", `/docs/assets/images/diceimages/dice${rollDice()}.png`);
+  const playerOneRoll = rollDice()
+  const playerTwoRoll = rollDice()
+  document.querySelector(".img1").setAttribute("src", `images/dice/${playerOneRoll}.png`)
+  document.querySelector(".img2").setAttribute("src", `images/dice/${playerTwoRoll}.png`);
+  if (playerOneRoll > playerTwoRoll) {
+    document.querySelector("#header").innerHTML = `${getPlayerOneName()} wins!`;
+  }
+  else if (playerTwoRoll > playerOneRoll) {
+    document.querySelector("#header").innerHTML = `${getPlayerTwoName()} wins!`;
+  }
+  else {
+    document.querySelector("#header").innerHTML = `It's a draw!`;
+  }
 }
 
 function rollDice() {
-  return Math.floor(Math.random() * 6) + 1;
+  return Math.floor(Math.random() * 20) + 1;
+}
+
+function getPlayerOneName() {
+  return document.querySelector(".one").textContent;
+}
+
+function getPlayerTwoName() {
+  return document.querySelector(".two").textContent;
 }
